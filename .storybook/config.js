3dffccdef67eb689a2893aa50e598c3b7148b192
Gianluca Esposito <g@esposi.to>
2018-04-17 11:14:35 +0200

    first commit

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+import { configure } from "@storybook/react";
+import { setOptions } from "@storybook/addon-options";
+
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "bootstrap-italia/dist/css/italia-icon-font.css";
+
+// addon-options
+// https://github.com/storybooks/storybook/tree/master/addons/options
+setOptions({
+  // name to display in the top left corner
+  name: "design-react-kit",
+  // URL for name in top left corner to link to
+  url: "https://github.com/italia/design-react-kit/"
+});
+
+// automatically import all files ending in *.stories.js
+const req = require.context("../stories", true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ README.md
@@ -0,0 +1,7 @@
+# design-react-kit
+
+`design-react-kit` estende il set di componenti di [reactstrap](https://reactstrap.github.io/) per Boostrap 4 ed implementa quelli presenti in [bootstrap-italia](https://italia.github.io/bootstrap-italia/).
+
+# Requisiti
+- npm@5
+
