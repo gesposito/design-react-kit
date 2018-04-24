579deaaf42bbb60706525034f059ecb3943b91d0
Gianluca Esposito <g@esposi.to>
2018-04-24 16:16:11 +0200

    Add StoryShots addon

---
+++ src/Storyshots.test.js
@@ -0,0 +1,5 @@
+// File is located here in order to avoid a custom jest.config.js
+// Current config is inherited from react-scripts
+import initStoryshots from "@storybook/addon-storyshots";
+
+initStoryshots();
